98b4a7acafaeb7648804034b467a44f1d60530f5
abrorjonDev <axmadov.git@gmail.com>
2024-10-23 17:20:56 +0500

    implemented

--- app/payments/validators.py
+++
@@ -1,9 +0,0 @@
-import re
-from django.core.exceptions import ValidationError
-
-
-def deadline_validator(value):
-    if not re.match(r'^(0[1-9]|1[0-2])\/\d{2}$', value):
-        return ValidationError(f"{value} is not a valid deadline for card")
-
-    return value
