f8c75ad3d53e342a372151d788f084dde1bb215d
abrorjonDev <axmadov.git@gmail.com>
2024-10-23 17:19:25 +0500

    implemented

---
+++ app/merchants/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class STATUS(models.TextChoices):
+    ACTIVE = 'active', _("Active")
+    SOON = 'soon', _("Soon") #  We are working on it, published as soon as possible.
+    INACTIVE = 'inactive', _("Inactive") # Inactive service, not found in global search
+
+
+class BaseModel(models.Model):
+    status = models.CharField(choices=STATUS.choices, max_length=10, default=STATUS.INACTIVE)
+    name_uz = models.CharField(max_length=255)
+    name_en = models.CharField(max_length=255)
+    name_ru = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+
+class MerchantCategory(BaseModel):
+    icon = models.JSONField(default=dict)
+
+    class Meta:
+        verbose_name = _("Merchant Category")
+        verbose_name_plural = _("Merchant Categories")
+
+    def __str__(self):
+        return self.name_uz or self.name_ru or self.name_en
+
+
+class Merchant(BaseModel):
+    image = models.ImageField(upload_to="images/merchants/", null=True, blank=True)
+
+    class Meta:
+        verbose_name = _("Merchant")
+        verbose_name_plural = _("Merchants")
+
+    def __str__(self):
+        return self.name_uz or self.name_ru or self.name_en
+
+
+class MerchantField(models.Model):
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="fields")
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+    required = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = (("merchant", "name"),)
+        verbose_name = _("Merchant")
+        verbose_name_plural = _("Merchants")
+        indexes = [models.Index(fields=["merchant"])]
---
+++ app/users/models.py
@@ -0,0 +1,78 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.utils.translation import gettext_lazy as _
+
+from users.managers import UserManager
+
+
+class Gender(models.TextChoices):
+    MALE = 'M', 'Male'
+    FEMALE = 'F', 'Female'
+    OTHER = 'O', 'Other'
+
+
+class User(AbstractUser):
+    username = None
+    phone_number = models.CharField(max_length=20, unique=True)
+    phone_number2 = models.CharField(max_length=20, unique=True)
+    birthdate = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=Gender.choices, max_length=1, null=True, blank=True)
+    image = models.ImageField(upload_to="images/profile/", null=True, blank=True)
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+class Device(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='devices')
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Device')
+        verbose_name_plural = _('Devices')
+        indexes = [
+            models.Index(fields=['user']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class ActionLog(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='actions')
+    action = models.CharField(max_length=500)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    created_at = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField()
+
+    class Meta:
+        verbose_name = _('Action Log')
+        verbose_name_plural = _('Action Logs')
+        indexes = [
+            models.Index(fields=['user']),
+        ]
+
+    def __str__(self):
+        return self.action
+
+
+class SMS(models.Model):
+    phone_number = models.CharField(max_length=20, unique=True)
+    message = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = _('SMS')
+        verbose_name_plural = _('SMSes')
+        indexes = [models.Index(fields=['phone_number'])]
+
+
+    def __str__(self):
+        return f"{self.message[:40]}"
