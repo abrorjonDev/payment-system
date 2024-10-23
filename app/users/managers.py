70d4284003c425539a8795c6590d9e7a6934493b
abrorjonDev <axmadov.git@gmail.com>
2024-10-23 17:23:33 +0500

    implemented

---
+++ app/users/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, phone_number, is_staff, is_superuser, **extra_fields):
+        if not phone_number:
+            raise ValueError('unique email must be required')
+
+        password = extra_fields.pop('password', None)
+
+        user = self.model(
+            phone_number=phone_number,
+            is_active=True,
+            is_staff=is_staff,
+            is_superuser=is_superuser,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone_number, **extra_fields):
+        return self._create_user(phone_number, False, False, **extra_fields)
+
+    def create_superuser(self, phone_number, **extra_fields):
+        return self._create_user(phone_number, True, True, **extra_fields)
