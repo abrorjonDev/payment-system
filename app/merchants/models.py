f3c14afcfd84a4f77381d8e6d7a20787d8362948
Abrorjon Axmadov <86520471+abrorjonDev@users.noreply.github.com>
2024-10-24 10:56:59 +0500

    Update models.py
    comment updated

--- app/merchants/models.py
+++ app/merchants/models.py
@@ -44,7 +44,7 @@ class Merchant(BaseModel):
 
 class MerchantField(models.Model):
     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="fields")
-    # TODO: need to add labels for rendering user cabinet
+    # TODO: need to add labels for rendering in merchant payment page
     name = models.CharField(max_length=255)
     value = models.CharField(max_length=255)
     required = models.BooleanField(default=False)
